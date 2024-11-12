f7e9f029a70592f040fa09d159dca5a5d0133908
murzilka3000 <168984297+murzilka3000@users.noreply.github.com>
2024-11-12 06:17:43 +0600

    favoirites

--- front/src/components/favorites/Favorites.tsx
+++ front/src/components/favorites/Favorites.tsx
@@ -2,15 +2,39 @@ import React from 'react';
 import { useFavorites } from '../../context/FavoritesContext';
 import productsData from '../../data/products.json';
 import { Product } from '../../types/Type';
+import { useNavigate } from 'react-router-dom';
+import { useCart } from '../../context/CartContext';
 
 const FavoritesList: React.FC = () => {
-    const { state } = useFavorites();
-    
+    const { state, dispatch: favoriteDispatch } = useFavorites();
+    const { dispatch: cartDispatch } = useCart();
+    const navigate = useNavigate();
+
+    // Находим избранные товары
     const favoriteProducts: Product[] = productsData.filter(product => state.items.includes(product.id));
 
     if (favoriteProducts.length === 0) {
         return <p>Your favorites list is empty.</p>;
     }
+
+    // Функция добавления товара в корзину и удаления из избранного
+    const handleAddToCart = (product: Product) => {
+      // Добавляем товар в корзину
+      cartDispatch({
+        type: 'ADD_TO_CART',
+        payload: {
+          productId: product.id,
+          quantity: 1,
+          price: product.price,
+          name: product.name,
+          image: product.image,
+        },
+      });
+      // Удаляем товар из избранного
+      favoriteDispatch({ type: 'REMOVE_FROM_FAVORITES', payload: product.id });
+      // Переход в корзину после добавления
+      navigate('/cart');
+    };
 
     return (
         <div>
@@ -21,6 +45,7 @@ const FavoritesList: React.FC = () => {
                         <h3>{product.name}</h3>
                         <p>{product.description}</p>
                         <p>Price: ${product.price}</p>
+                        <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
                     </li>
                 ))}
             </ul>
